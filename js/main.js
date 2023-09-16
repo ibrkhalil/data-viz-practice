bc7fd13e7da3b6d66a598f3169f2af4541142b84
ibrkhalil <vampirekid017@gmail.com>
2023-09-16 17:40:31 +0300

    Initial approach

--- js/main.js
+++ js/main.js
@@ -3,20 +3,19 @@
 *    Mastering Data Visualization with D3.js
 *    Project 3 - CoinStats
 */
-		
-const MARGIN = { LEFT: 20, RIGHT: 100, TOP: 50, BOTTOM: 100 }
-const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT
-const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
 
+const MARGIN = { LEFT: 90, RIGHT: 100, TOP: 50, BOTTOM: 100 }
+const WIDTH = 860 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
 const svg = d3.select("#chart-area").append("svg")
-  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+	.attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+	.attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
 
 const g = svg.append("g")
-  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+	.attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
 // time parser for x-scale
-const parseTime = d3.timeParse("%Y")
+const parseTime = d3.timeParse('%d/%m/%Y')
 // for tooltip
 const bisectDate = d3.bisector(d => d.year).left
 
@@ -24,11 +23,19 @@ const bisectDate = d3.bisector(d => d.year).left
 const x = d3.scaleTime().range([0, WIDTH])
 const y = d3.scaleLinear().range([HEIGHT, 0])
 
+// xAxisGroup
+const xAxisGroup = g.append("path")
+	.attr("class", "line")
+	.attr("fill", "none")
+	.attr("stroke", "grey")
+	.attr("stroke-width", "3px")
+
 // axis generators
 const xAxisCall = d3.axisBottom()
+const valueFormatter = d3.format(".2s")
 const yAxisCall = d3.axisLeft()
 	.ticks(6)
-	.tickFormat(d => `${parseInt(d / 1000)}k`)
+	.tickFormat(d => valueFormatter(d).replace(/G/, "B"))
 
 // axis groups
 const xAxis = g.append("g")
@@ -36,47 +43,58 @@ const xAxis = g.append("g")
 	.attr("transform", `translate(0, ${HEIGHT})`)
 const yAxis = g.append("g")
 	.attr("class", "y axis")
-    
+
+let coinSelect = $('#coin-select')
+let varSelect = $('#var-select')
+let selectedCoin = coinSelect.find(":selected").val();
+let selectedVar = varSelect.find(":selected").val();
+
+let selectedVarToYTitle = (selectedVar) => {
+	switch (selectedVar) {
+		case "price_usd":
+			return "Price ($)"
+		case "market_cap":
+			return "Market Capitalization ($)"
+		case "24h_vol":
+			return "24 Hour Trading Volume ($)"
+	}
+}
+
 // y-axis label
-yAxis.append("text")
-	.attr("class", "axis-title")
-	.attr("transform", "rotate(-90)")
+yAxisLabel = yAxis.append("text")
+	.attr("class", "y-axis-title")
 	.attr("y", 6)
-	.attr("dy", ".71em")
+	.attr("dy", "-4em")
+	.attr("dx", "-4.5em")
+	.attr("transform", "rotate(-90)")
 	.style("text-anchor", "end")
 	.attr("fill", "#5D6971")
-	.text("Population)")
+
+// x-axis label
+xAxis.append("text")
+	.attr("class", "x-axis-title")
+	.attr("transform", `translate(${WIDTH / 2}, 0)`)
+	.style("text-anchor", "start")
+	.attr("dy", "4em")
+	.attr("fill", "#5D6971")
+	.text("Time")
 
 // line path generator
 const line = d3.line()
 	.x(d => x(d.year))
 	.y(d => y(d.value))
 
-d3.json("data/example.json").then(data => {
+d3.json("data/coins.json").then(data => {
 	// clean data
-	data.forEach(d => {
-		d.year = parseTime(d.year)
-		d.value = Number(d.value)
+	coinSelect.on('change', function () {
+		selectedCoin = this.value
+		update(data)
 	})
 
-	// set scale domains
-	x.domain(d3.extent(data, d => d.year))
-	y.domain([
-		d3.min(data, d => d.value) / 1.005, 
-		d3.max(data, d => d.value) * 1.005
-	])
-
-	// generate axes once scales have been set
-	xAxis.call(xAxisCall.scale(x))
-	yAxis.call(yAxisCall.scale(y))
-
-	// add line to chart
-	g.append("path")
-		.attr("class", "line")
-		.attr("fill", "none")
-		.attr("stroke", "grey")
-		.attr("stroke-width", "3px")
-		.attr("d", line(data))
+	varSelect.on('change', function () {
+		selectedVar = this.value
+		update(data)
+	})
 
 	/******************************** Tooltip Code ********************************/
 
@@ -111,15 +129,43 @@ d3.json("data/example.json").then(data => {
 
 	function mousemove() {
 		const x0 = x.invert(d3.mouse(this)[0])
-		const i = bisectDate(data, x0, 1)
-		const d0 = data[i - 1]
-		const d1 = data[i]
+		const i = bisectDate(data[selectedCoin], x0, 1)
+		const d0 = data[selectedCoin][i - 1]
+		const d1 = data[selectedCoin][i]
 		const d = x0 - d0.year > d1.year - x0 ? d1 : d0
 		focus.attr("transform", `translate(${x(d.year)}, ${y(d.value)})`)
-		focus.select("text").text(d.value)
+		focus.select("text").text(valueFormatter(d.value).replace(/G/, "B"))
 		focus.select(".x-hover-line").attr("y2", HEIGHT - y(d.value))
 		focus.select(".y-hover-line").attr("x2", -x(d.year))
 	}
-	
+	update(data)
+
 	/******************************** Tooltip Code ********************************/
 })
+
+const update = (data) => {
+	const transition = d3.transition()
+	.duration(1000)
+	// set scale domains
+	data[selectedCoin].map(d => {
+		d.year = parseTime(d.date)
+		d.value = Number(d[selectedVar])
+	})
+	x.domain(d3.extent(data[selectedCoin], d => d.year))
+	y.domain([
+		d3.min(data[selectedCoin], d => d.value) / 1.005,
+		d3.max(data[selectedCoin], d => d.value) * 1.005
+	])
+
+	// xAxisGroup.attr("d", line(data[selectedCoin]))
+
+	// generate axes once scales have been set
+	xAxis.transition(transition).call(xAxisCall.scale(x))
+	yAxis.transition(transition).call(yAxisCall.scale(y))
+
+	yAxisLabel.text(selectedVarToYTitle(selectedVar))
+
+	g.selectAll(".line")
+		.transition(transition)
+		.attr("d", line(data[selectedCoin]))
+}
